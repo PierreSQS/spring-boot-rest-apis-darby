b3411ab47fe75642698ba2c6a00cd5b1056e45aa
PierreSQS <nomo_messina@web.de>
2025-05-17 15:19:46 +0200

    Sec5_Chap95-XXX: changes due to SB-Upgrade to 3.4.5

--- 03-spring-boot-rest-security/employees/src/test/java/com/luv2code/springboot/employees/controller/EmployeeRestControllerSecurityTest.java
+++ 03-spring-boot-rest-security/employees/src/test/java/com/luv2code/springboot/employees/controller/EmployeeRestControllerSecurityTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -26,7 +26,7 @@ class EmployeeRestControllerSecurityTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     EmployeeService employeeService;
 
     @Test
--- 03-spring-boot-rest-security/employees/src/test/java/com/luv2code/springboot/employees/controller/EmployeeRestControllerTest.java
+++ 03-spring-boot-rest-security/employees/src/test/java/com/luv2code/springboot/employees/controller/EmployeeRestControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -30,7 +31,7 @@ class EmployeeRestControllerTest {
     @Autowired
     ObjectMapper objectMapper;
     
-    @MockBean
+    @MockitoBean
     EmployeeService employeeService;
 
     @Test
