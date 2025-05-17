194a8a05a01c2e00209852eed23e73da53fd057d
PierreSQS <nomo_messina@web.de>
2025-05-17 14:35:00 +0200

    Sec5_Chap95-XXX: introduced lombok in entities and DTOS

--- 03-spring-boot-rest-security/employees/src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
+++ 03-spring-boot-rest-security/employees/src/main/java/com/luv2code/springboot/employees/request/EmployeeRequest.java
@@ -3,7 +3,17 @@ package com.luv2code.springboot.employees.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeRequest {
 
     @NotBlank(message = "First name is mandatory")
@@ -18,33 +28,4 @@ public class EmployeeRequest {
     @Email(message = "Please provide a valid email address")
     private String email;
 
-    public EmployeeRequest(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
