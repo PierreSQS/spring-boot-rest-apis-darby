3e4c6fe432f290008b228eef254b239ca1595425
PierreSQS <nomo_messina@web.de>
2025-05-18 19:10:21 +0200

    Sec6_Chap114: implemented the Security User's functionalities

--- 04-todos/src/main/java/com/luv2code/springboot/todos/entity/SecurityUser.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/entity/SecurityUser.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @RequiredArgsConstructor
 public class SecurityUser implements UserDetails {
@@ -14,16 +13,16 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return user.getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getEmail();
     }
 }
