6fb4e8e6a96bd3498280c9d05950654d65122292
PierreSQS <nomo_messina@web.de>
2025-05-18 23:34:19 +0200

    Sec6_Chap124: implemented generate JWT-Token

--- 04-todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
@@ -1,15 +1,16 @@
 package com.luv2code.springboot.todos.service;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
 import java.util.Map;
 
 @Service
 public class JwtServiceImpl implements JwtService {
-    @Value("${jwt.secret}")
-    private String jwtSecret;
 
     @Value("${jwt.expiration}")
     private String jwtExpiration;
@@ -26,7 +27,14 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
-        return "";
+        // Create a secret key using the signature algorithm
+        SecretKey jwtSecret = Jwts.SIG.HS256.key().build();
+        return Jwts.builder()
+                .claims(claims).subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + Long.parseLong(jwtExpiration)))
+                .signWith(jwtSecret)
+                .compact();
     }
 
     @Override
