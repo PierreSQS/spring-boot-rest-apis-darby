db64453ea2dc9b57f2079ec3d38371c229272c6f
PierreSQS <nomo_messina@web.de>
2025-05-19 00:04:32 +0200

    Sec6_Chap125: implemented extract Token

--- 04-todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
@@ -1,7 +1,7 @@
 package com.luv2code.springboot.todos.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtServiceImpl implements JwtService {
@@ -18,7 +19,7 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String extractUsername(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
     }
 
     @Override
@@ -39,6 +40,21 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public boolean isTokenExpired(String token, UserDetails userDetails) {
         return false;
+    }
+
+    // Extract a specific claim from all the claims
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    // Extract all claims from the token
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     // Create a secret key using the signature algorithm
