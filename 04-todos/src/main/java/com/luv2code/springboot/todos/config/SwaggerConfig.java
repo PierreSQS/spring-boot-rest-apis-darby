b0464bed8e667fc148e5ee372e452afb44e376de
PierreSQS <nomo_messina@web.de>
2025-06-04 23:25:12 +0200

    Sec6_Chap142: implemented SwaggerConfig for Bearer Token

---
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.todos.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ToDos API",
+                version = "1.0",
+                description = "# API for managing todos"
+        ),
+        security = @SecurityRequirement(
+                name = "bearerAuth"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
