dee64331aaa366034a50b40bf880f7579bdd4001
PierreSQS <nomo_messina@web.de>
2025-06-05 23:58:04 +0200

    Sec6_Chap143-XXX: workaround for JSON-Serialization of SecurityUser

--- 04-todos/src/main/java/com/luv2code/springboot/todos/entity/SecurityUser.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/entity/SecurityUser.java
@@ -11,6 +11,22 @@ public class SecurityUser implements UserDetails {
 
     private final User user;
 
+    public Long getId() {
+        return user.getId();
+    }
+
+    public String getFirstName() {
+        return user.getFirstName();
+    }
+
+    public String getLastName() {
+        return user.getLastName();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return user.getAuthorities();
