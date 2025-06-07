0778a7b45da2af537c2fd4557579db5719f11469
PierreSQS <nomo_messina@web.de>
2025-06-07 09:21:23 +0200

    Sec6_Chap147: implemented deleteUser()-handler

--- 04-todos/src/main/java/com/luv2code/springboot/todos/controller/UserController.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/controller/UserController.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.todos.response.UserResponse;
 import com.luv2code.springboot.todos.userservice.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
     @GetMapping("/info")
     public UserResponse getUserInfo() {
         return userService.getUserInfo();
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteUser() {
+        userService.deleteUser();
     }
 
 
