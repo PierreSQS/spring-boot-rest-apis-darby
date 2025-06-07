8809f2daf8edd8a9dc02893ca73ef09915ac23f7
PierreSQS <nomo_messina@web.de>
2025-06-07 09:06:51 +0200

    Sec6_Chap146: custom query to count the admins

--- 04-todos/src/main/java/com/luv2code/springboot/todos/repository/UserRepository.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.luv2code.springboot.todos.repository;
 
 import com.luv2code.springboot.todos.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT COUNT(u) FROM User u JOIN u.authorities a WHERE a.authority = 'ROLE_ADMIN'")
+    int countAdmins();
 }
+
