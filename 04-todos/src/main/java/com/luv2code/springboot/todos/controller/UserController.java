c3ce3787db1baf17daabbeaaab20858fcded9ea8
PierreSQS <nomo_messina@web.de>
2025-06-08 09:30:45 +0200

    Sec6_Chap155: implemented updatePassword()-handler

--- 04-todos/src/main/java/com/luv2code/springboot/todos/controller/UserController.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.luv2code.springboot.todos.controller;
 
+import com.luv2code.springboot.todos.request.PasswordUpdateRequest;
 import com.luv2code.springboot.todos.response.UserResponse;
 import com.luv2code.springboot.todos.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,9 @@ import org.springframework.web.bind.annotation.RestController;
         userService.deleteUser();
     }
 
+    @PutMapping("/update-password")
+    public void updatePassword(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest) {
+        userService.updatePassword(passwordUpdateRequest);
+    }
 
 }
