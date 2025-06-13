2832145081b1cc709fe548edf78e9114fcf1f200
PierreSQS <nomo_messina@web.de>
2025-06-13 14:01:28 +0200

    Sec6_Chap160: implemented Todo Controller

---
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/controller/TodoController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.todos.controller;
+
+import com.luv2code.springboot.todos.request.TodoRequest;
+import com.luv2code.springboot.todos.response.TodoResponse;
+import com.luv2code.springboot.todos.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Todo REST API Endpoints", description = "Operations related to managing current authenticated user todos.")
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
+@RestController
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Operation(summary = "Create a new Todo", description = "Create a new Todo for the authenticated user.")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public TodoResponse createTodo(@Valid @RequestBody TodoRequest todoRequest) {
+        return todoService.createTodo(todoRequest);
+    }
+}
