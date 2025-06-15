1fd8e6946fb392fc0b83e6065883be7d79f3669b
PierreSQS <nomo_messina@web.de>
2025-06-15 09:21:36 +0200

    Sec6_Chap167: completed the TodoController's implementation

--- 04-todos/src/main/java/com/luv2code/springboot/todos/controller/TodoController.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/controller/TodoController.java
@@ -6,13 +6,20 @@ import com.luv2code.springboot.todos.service.TodoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "Todo REST API Endpoints", description = "Operations related to managing current authenticated user todos.")
 @RequiredArgsConstructor
@@ -22,10 +29,32 @@ public class TodoController {
 
     private final TodoService todoService;
 
-    @Operation(summary = "Create a new Todo", description = "Create a new Todo for the authenticated user.")
+    @Operation(summary = "Get all ToDos", description = "Fetch all ToDos for the authenticated user.")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<TodoResponse> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @Operation(summary = "Create a new ToDo", description = "Create a new ToDo for the authenticated user.")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public TodoResponse createTodo(@Valid @RequestBody TodoRequest todoRequest) {
         return todoService.createTodo(todoRequest);
     }
+
+    @Operation(summary = "Toggle ToDo completion by ID", description = "Toggle the completion status of a ToDo for the authenticated user.")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    public TodoResponse toggleTodoCompletion(@PathVariable @Min(1) Long id) {
+        return todoService.toggleTodoCompletion(id);
+    }
+
+    @Operation(summary = "Delete ToDo by ID", description = "Delete a ToDo for the authenticated user.")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable @Min(1) Long id) {
+        todoService.deleteTodo(id);
+    }
+
 }
