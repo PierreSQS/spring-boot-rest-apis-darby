719a0f400f53f75ebec85ab9307b15e30734c3a7
PierreSQS <nomo_messina@web.de>
2025-09-23 20:28:08 +0200

    Sec3_XXX: optimized test (sb356)

--- 02-books-continued/src/test/java/com/luv2code/books/BooksApplicationTests.java
+++ 02-books-continued/src/test/java/com/luv2code/books/BooksApplicationTests.java
@@ -2,12 +2,16 @@ package com.luv2code.books;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BooksApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
 	}
 
 }
