a3041b8ac0c8492c2ad9d36d343702ebd43bd50d
PierreSQS <nomo_messina@web.de>
2025-10-05 14:57:15 +0200

    Sec6_Chap173-XXX: optimized filter (sb356)

--- 04-todos/src/main/java/com/luv2code/springboot/todos/config/JwtAuthenticationFilter.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/config/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -38,11 +39,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
 
@@ -62,6 +58,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(@NonNull HttpServletRequest request) {
+        return !StringUtils.hasText(request.getHeader("Authorization"));
     }
 }
 
