058ab2efcdc58e5b47d9de70bec012a0f47d9fb2
PierreSQS <nomo_messina@web.de>
2026-09-04 12:35:22 +0200

    Sec6_Chap173-XXX: added null check for JWT authorization header
    Note: we re-introduced the null check in the header in commit a3041b8a for performance reasons

--- 04-todos/src/main/java/com/luv2code/springboot/todos/config/JwtAuthenticationFilter.java
+++ 04-todos/src/main/java/com/luv2code/springboot/todos/config/JwtAuthenticationFilter.java
@@ -39,6 +39,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
 
